f4b2bb015c5d3b9d6b758510aa36ef0cc72d2f0b
Toxa <p07a1330@yandex.ru>
2018-07-22 15:29:33 +0300

    try to fix coruch

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,66 +4,55 @@ public class PlayerControl : MonoBehaviour {
 
     public Animator animator;
     public Rigidbody2D rigidbody;
-    public Transform groundCheck;
-    public LayerMask groundMask;
+    public BoxCollider2D hitbox; // on triger check if attacked
+    public BoxCollider2D groundCollider; // ground collider
+    public BoxCollider2D runCollider; // controls wall collisions while running
 
     private float jumpDirection;
     private float direction;
 
+    private float playerHeight;
+    private float idleHeight;
+    private float playerWidth;
+
     private int speed = 20;
-    private int jumpSpeed = 100;
-    private float scoperLen = 3;
-    private float scoperAng = 0;
-    private float scopeRotSpeed = 1;
-    private float scopeMoveSpeed = 0.1f;
-    private float scopeMaxLen = 50;
-    public float fireForce = 100000;
-    public float playerHeight;
-    Rigidbody2D bullet;
+    private int jumpForce = 2500;
+    private float crouchSlowDown = 0.75f;
 
     private bool isFacingRight = true;
-    GameObject scoper;
     private bool attacks;
 
-
-    private Rigidbody2D curSlot, slot1, slot2, slot3;
-    public Rigidbody2D weapon1, weapon2, weapon3;
-    public Rigidbody2D scope;
+    public bool isGrounded;
 
     private Vector2 moveVector = new Vector2();
 
-    private enum BaseState {
+    public enum BaseState {
         IDLE,
         MOVE
     }
 
-    private enum AdditionalState {
+    public enum AdditionalState {
         NONE,
         JUMP,
-        SIT
+        CROUCH
     }
 
-    private BaseState baseState;
-    private AdditionalState additionalState;
+    public BaseState baseState;
+    public AdditionalState additionalState;
 
     private void Start() {
-        playerHeight = GetComponent<SpriteRenderer>().size.y * transform.localScale.y;
-        scoper = Instantiate(scope.gameObject, new Vector2(0, 0), Quaternion.identity);
-        slot1 = weapon1; slot2 = weapon2; slot3 = weapon3;
-        curSlot = slot1;
+        playerHeight = idleHeight = hitbox.size.y;
     }
 
     void FixedUpdate() {
+        changeColliderSize();
         handleInput();
         handleState();
-        aiming();
-
-
+        isPossStand();
     }
 
     private void handleInput() {
-        movement();
-        aiming();
+        handleMoveInput();
     }
 
     private void handleState() {
@@ -74,22 +63,27 @@ public class PlayerControl : MonoBehaviour {
         handleAdditionalState();
         handleBaseState();
 
+        moveVector.y = rigidbody.velocity.y;
         rigidbody.velocity = moveVector;
-        if (nearTheWall(isGrounded()) && !isGrounded())
-            rigidbody.AddForce(Vector2.down * 1000);
     }
 
     private void handleAdditionalState() {
         if (additionalState == AdditionalState.JUMP)
             handleJump();
+        if (additionalState == AdditionalState.CROUCH)
+            handleCrouch();
+        else
+            playerHeight = idleHeight;
     }
 
     private void handleJump() {
         jumpDirection = direction;
-        if (isGrounded() && !attacks) 
-            moveVector.y = jumpSpeed;
-        else
-            moveVector.y = 0;
+        if (isGrounded && !attacks)
+            rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Force);
+    }
+
+    private void handleCrouch() {
+        playerHeight = GetComponent<SpriteRenderer>().bounds.size.y / transform.lossyScale.y;
     }
 
     private void handleBaseState() {
@@ -112,75 +106,60 @@ public class PlayerControl : MonoBehaviour {
         attacks = false;
     }
 
-    bool isGrounded()
-    {
-        RaycastHit2D hit_c = Physics2D.Raycast(rigidbody.position, Vector2.down, playerHeight / 2.0f, groundMask);
-        RaycastHit2D hit_r = Physics2D.Raycast(rigidbody.position + new Vector2(GetComponent<SpriteRenderer>().size.x, 0) / 2.0f, Vector2.down, playerHeight / 2.0f, groundMask);
-        RaycastHit2D hit_l = Physics2D.Raycast(rigidbody.position - new Vector2(GetComponent<SpriteRenderer>().size.x, 0) / 2.0f, Vector2.down, playerHeight / 2.0f, groundMask);
-
-        if ((hit_c.collider != null) || (hit_l.collider != null) || (hit_r.collider != null))
-            return true;
-        return false;
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Enemy")
+            isGrounded = true;     
     }
 
-    bool nearTheWall(bool isGr)
+    void OnTriggerStay2D(Collider2D other)
     {
-        RaycastHit2D hit_r = Physics2D.Raycast(rigidbody.position, Vector2.right, GetComponent<SpriteRenderer>().size.x* transform.localScale.x / 2f, groundMask);
-        RaycastHit2D hit_l = Physics2D.Raycast(rigidbody.position, Vector2.left, GetComponent<SpriteRenderer>().size.x* transform.localScale.x / 2f, groundMask);
-        if ((hit_l.collider != null) || (hit_r.collider != null))
-        {
-            return true;
-        }
-        return false;
+        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Enemy")
+            isGrounded = true;
     }
 
-    void aiming()
-    {
-
-        if (Input.GetKeyDown(KeyCode.F))
-            attacks = true;
-
-        if (isFacingRight)
-            scoper.transform.position = new Vector2(this.rigidbody.transform.position.x + scoperLen * Mathf.Cos(scoperAng * 0.0174533f), this.rigidbody.transform.position.y + scoperLen * Mathf.Sin(scoperAng * 0.0174533f));
-        if (!isFacingRight)
-            scoper.transform.position = new Vector2(rigidbody.transform.position.x - scoperLen * Mathf.Cos(scoperAng * 0.0174533f), rigidbody.transform.position.y + scoperLen * Mathf.Sin(scoperAng * 0.0174533f));
-        if ((Input.GetKey(KeyCode.Q) && (scoperAng < 90)))
-            scoperAng+=scopeRotSpeed;
-        if ((Input.GetKey(KeyCode.E) && (scoperAng > -90)))
-            scoperAng-=scopeRotSpeed;
-        if ((Input.GetKey(KeyCode.LeftBracket) && (scoperLen < scopeMaxLen)))
-            scoperLen += scopeMoveSpeed;
-        if ((Input.GetKey(KeyCode.RightBracket) && (scoperLen > 1)))
-            scoperLen -= scopeMoveSpeed;
-
-
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-            curSlot = slot1;
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-            curSlot = slot2;
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-            curSlot = slot3;
-
-        if ((Input.GetKeyDown(KeyCode.G)) && (isFacingRight))
-            Instantiate(curSlot, new Vector2(this.transform.position.x+playerHeight/2.0f, this.transform.position.y), Quaternion.identity).AddForce((Vector2.up * Mathf.Sin(scoperAng * 0.0174533f) + Vector2.right * Mathf.Cos(scoperAng * 0.0174533f)) * fireForce);
-        if ((Input.GetKeyDown(KeyCode.G)) && (!isFacingRight))
-            Instantiate(curSlot, new Vector2(this.transform.position.x-playerHeight/2.0f, this.transform.position.y), Quaternion.identity).AddForce((Vector2.up * Mathf.Sin(scoperAng * 0.0174533f) + Vector2.left * Mathf.Cos(scoperAng * 0.0174533f)) * fireForce);
-      
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "Enemy")
+        isGrounded = false;
     }
 
-    void movement()
+    private void handleMoveInput()
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
             baseState = BaseState.MOVE;
         else
             baseState = BaseState.IDLE;
 
-        if (Input.GetKeyDown(KeyCode.Space) || !isGrounded())
+        if ((Input.GetKeyDown(KeyCode.Space) || !isGrounded) && additionalState != AdditionalState.CROUCH)
             additionalState = AdditionalState.JUMP;
-        else if (Input.GetKey(KeyCode.LeftControl) && isGrounded())
-            additionalState = AdditionalState.SIT;
-        else if (isGrounded())
+        else if (Input.GetKey(KeyCode.C) && isGrounded)
+            additionalState = AdditionalState.CROUCH;
+        else if (isGrounded && isPossStand())
             additionalState = AdditionalState.NONE;
+    }
 
+    private void changeColliderSize() {
+        playerWidth = System.Math.Abs(GetComponent<SpriteRenderer>().bounds.size.x / transform.lossyScale.x);
+        hitbox.size = new Vector2(playerWidth, playerHeight);
+        groundCollider.size = new Vector2(Mathf.Max(playerWidth, runCollider.size.x) * 0.98f, groundCollider.size.y);
+        groundCollider.offset = new Vector2(0, -hitbox.size.y / 2);
+
+        runCollider.size = new Vector2(runCollider.size.x, playerHeight);
+    }
+
+    bool isPossStand()
+    {
+        Debug.DrawRay(rigidbody.position, Vector2.up, Color.red, 0.1f);
+        Debug.DrawRay(rigidbody.position + new Vector2(playerWidth,0) / 2.0f, Vector2.up * (idleHeight / 2 + (idleHeight - playerHeight) / 2), Color.red, 0.1f);
+        Debug.DrawRay(rigidbody.position + new Vector2(-playerWidth, 0) / 2.0f, Vector2.up*(idleHeight/2+ (idleHeight-playerHeight)/2), Color.red, 0.1f);
+
+
+        RaycastHit2D hit_c = Physics2D.Raycast(rigidbody.position, Vector2.up, (idleHeight / 2 + (idleHeight - playerHeight) / 2));
+        RaycastHit2D hit_r = Physics2D.Raycast(rigidbody.position + new Vector2(-playerWidth, 0) / 2.0f, Vector2.up, (idleHeight / 2 + (idleHeight - playerHeight) / 2));
+            RaycastHit2D hit_l = Physics2D.Raycast(rigidbody.position + new Vector2(+playerWidth, 0) / 2.0f, Vector2.up, (idleHeight / 2 + (idleHeight - playerHeight) / 2));
+
+        if ((hit_c.collider == null) && (hit_l.collider == null) && (hit_r.collider == null))
+                return true;
+            return false;
+        
     }
 }
