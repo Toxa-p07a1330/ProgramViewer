a9fc6ef37d4eeb1df672f424149a7a10844ab6e6
Toxa <p07a1330@yandex.ru>
2018-07-24 11:40:52 +0300

    added enemy vision

--- Assets/CustomAssests/Scripts/CameraController.cs
+++ Assets/CustomAssests/Scripts/CameraController.cs
@@ -13,13 +13,13 @@ public class CameraController : MonoBehaviour {
     private void Start()
     {
         AttackCntr = GetComponent<AttackController>();
+        
     }
 
     private void Update()
     {
-        Vector3 newPosition = new Vector3((scope.transform.position.x + target.transform.position.x) / 2.0f, (scope.transform.position.y + target.transform.position.y) / 2.0f, -100);
-        newPosition.z = -10;
         scope = GameObject.Find("Scope(Clone)");
+        Vector3 newPosition = new Vector3((scope.transform.position.x + target.transform.position.x) / 2.0f, (scope.transform.position.y + target.transform.position.y) / 2.0f, -50);
         transform.position = Vector3.Slerp(this.transform.position, newPosition, FollowSpeed * Time.deltaTime);
         //transform.position = new Vector3((scope.transform.position.x + this.transform.position.x)/2.0f, (scope.transform.position.y + this.transform.position.y) / 2.0f, -100);//scope.transform.position;
 
--- Assets/CustomAssests/Scripts/EnemyController.cs
+++ Assets/CustomAssests/Scripts/EnemyController.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float VisionLen = 10f;
+    public float maxAngleUp = 30;
+    public float maxAngleDown = -30;
+    public float rotationSpeed = 1f;
+    int isMovingUp = 1;
+    public float angle = 0;
+    public int direction = 1; //1 if enemy is looking rigt, -1 if is looking left
+    private const float angleRealtion = 0.0174533f;
+    RaycastHit2D vision;
+    public bool see;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        checkViewArea();
+
+        Debug.DrawRay(this.transform.position, new Vector3(VisionLen * Mathf.Cos(angle * angleRealtion) * direction, VisionLen * Mathf.Sin(angle * angleRealtion)));
+        vision = Physics2D.Raycast(this.transform.position, new Vector3(VisionLen * Mathf.Cos(angle * angleRealtion) * direction, VisionLen * Mathf.Sin(angle * angleRealtion)));
+        see = false;
+        if (vision && vision.distance<=VisionLen)
+        {
+            if(vision.collider.tag == "Player")
+                 see = true;
+        }
+        		
 	}
+
+    void checkViewArea()
+    {
+
+        if (true)
+        {
+            angle += rotationSpeed*isMovingUp;
+        }
+        if ((angle>=maxAngleUp)||(angle<=maxAngleDown))
+        {
+            isMovingUp *= -1;
+        }
+
+
+    }
 }
